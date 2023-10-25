7d696d2f93716dee54e4c6e1769577674ba52ba1
off0405 <off0405@naver.com>
2023-10-25 19:42:12 +0900

    로컬스토리지 저장

--- 11_React/my-todo/src/App.js
+++ 11_React/my-todo/src/App.js
@@ -3,8 +3,9 @@ import reset, { Reset } from "styled-reset";
 import TodoTemplate from "./components/TodoTemplate";
 import TodoInsert from "./components/TodoInsert";
 import TodoListItem from "./components/TodoListItem";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import TodoList from "./components/TodoList";
+import { v4 as uuidv4 } from "uuid";
 
 
 // 패키지 설치
@@ -58,6 +59,18 @@ function App() {
     },
   ]);
 
+  //  로컬스토리지에서 가져오기
+  
+
+  // 로컬 스토리지에 저장(주의: DB가 아니다. DB처럼 쓰면 안된다)
+  // 추가, 수정, 삭제 각 함수에 넣어도 되지만, useEffect()를 활용하면 한번에 처리 가능 !
+  // todos가 변경될 때 마다 실행..........
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos])
+
+
+
   // todos 배열에 새 객체를 추가하기 위한 함수 정의 -------------------------------
   // 새 객체를 만들때 마다 id값에 1씩 더해줘야 하는데 useRef를 사용하여 변수 생성
   // id 값은 렌더링되는 정보가 아님(화면에 보이지도 않고, 값이 바뀐다고 해서 컴포넌트가 재렌더링 될 필요도 없어서 useRef()사용 할 거야~)
@@ -65,7 +78,8 @@ function App() {
 
   const handleInsert = (text) => {
     const todo = {
-      id: nextId.current,
+      // id: nextId.current,
+      id: uuidv4(),
       text, // text: text
       checked: false
     }
@@ -137,3 +151,22 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+// HTML 웹 스토리지란?
+// 브라우저에서 제공하는 데이터 저장소
+// 사용자의 브라우저 내에 로컬로 데이터를 저장할 수 있음
+// key-value 형태로 저장
+// 최대 5MB까지 문자만 저장가능
+// 콘솔 창에서 연습해보기
+
+// 웹 스토리지는 origin(도메인 및 프로토콜)당입니다. 
+// 같은 출처의 모든 페이지는 동일한 데이터를 저장하고 액세스할 수 있습니다.
+
+// HTML 웹 스토리지 객체
+// HTML 웹 스토리지는 클라이언트에 데이터를 저장하기 위한 두 가지 객체를 제공합니다.
+// window.localStorage - 만료 날짜 없이 데이터를 저장
+// window.sessionStorage - 한 세션에 대한 데이터 저장(브라우저 탭을 닫으면 데이터가 손실됨)
