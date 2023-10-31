b3c7f559c2ae8b80153ef779dd076494c360866d
off0405 <off0405@naver.com>
2023-10-31 20:33:06 +0900

    newsItem

---
+++ 11_React/my-news/src/components/NewsList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+// API를 요청하고,  뉴스 데이터가 들어있는 배열을 리액트 컴포넌트 배열로 변환하여 렌더링하는 컴포넌또~
+
+// 스타일
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+function NewsList(props) {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default NewsList;
