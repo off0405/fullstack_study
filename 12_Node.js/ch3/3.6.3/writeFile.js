0f0b89b1adc730705f3ab570b66fdfa0a847b2c3
off0405 <off0405@naver.com>
2023-11-21 15:51:55 +0900

    sync

--- 12_Node.js/ch3/3.6.3/writeFile.js
+++ 12_Node.js/ch3/3.6.3/writeFile.js
@@ -2,4 +2,20 @@ const fs = require('fs').promises
 
 // 파일 생성
 // 인자값: 만들 파일의 경로와 파일명, 파일에 작성할 내용
-fs.writeFile('./writeme.txt', '글이 입력됩니당');
+// fs.writeFile('./writeme.txt', '글이 입력됩니당')
+//   .then(() => {
+//     return fs.readFile('./writeme.txt') // 파일을 만든 후 바로 읽어보기
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     console.log(data.toString());
+//   })
+//   .catch((err) => { console.error(err); })
+
+async function fsWriteFile() {
+  await fs.writeFile('./writeme.txt', '글이 입력됩니다다다')
+  const data = await fs.readFile('./writeme.txt')
+  console.log(data.toString());
+}
+
+fsWriteFile()
