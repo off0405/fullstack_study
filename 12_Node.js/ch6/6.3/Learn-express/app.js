f62d2a43064da6f040058ed2e32f86b2f407ca25
off0405 <off0405@naver.com>
2023-11-28 17:12:30 +0900

    미들웨어 분리

--- 12_Node.js/ch6/6.3/Learn-express/app.js
+++ 12_Node.js/ch6/6.3/Learn-express/app.js
@@ -12,24 +12,12 @@ const app = express();
 // 초기 세팅
 app.set('port', process.env.PORT || 3000)
 
-// morgan
-app.use(morgan('dev'));
-
-// static: 동적인 변동 사항이 없는 정적 파일들 (예: css, js, image 등)
-// 폴더를 서버에 등록해두면 폴더 안의 정적 파일들을 사용 가능하게
-app.use('/', express.static(path.join(__dirname, 'public')))
-
-
-// cookie-parser
-app.use(cookieParser(process.env.COOKIE_SECRET))
-
-
-// body-parser
-app.use(express.json())
+app.use(morgan('dev')); // morgan
+app.use('/', express.static(path.join(__dirname, 'public'))) // static: 폴더를 서버에 등록해두면 폴더 안의 정적 파일들을 사용 가능하게
+app.use(cookieParser(process.env.COOKIE_SECRET)) // cookie-parser
+app.use(express.json()) // body-parser
 app.use(express.urlencoded({ extended: true }))
-
-// express-session: 요청마다 개인의 저장 공간을 만들어주는 세션 관리용 미들웨어
-app.use(session({
+app.use(session({ // express-session: 요청마다 개인의 저장 공간을 만들어주는 세션 관리용 미들웨어
   resave: false,
   saveUninitialized: false,
   secret: process.env.COOKIE_SECRET,
@@ -38,15 +26,24 @@ app.use(session({
   },
   name: 'cookie-cookie'
 }))
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
+app.use((req, res, next) => { // 404 에러 처리
+  res.status(404).send('404 Not Found')
 })
 
-// 404 에러 처리
-app.use((req, res, next) => {
-  res.status(404).send('404 Not Found')
+
+// 라우터 분리하기 
+// 개발을 하다보면 app.get(),  app.post() 등이 계속 늘어나는데
+// 이걸 app.js같은 하나의 파일에 계속 쓰다보면 코드가 길어지고 복잡해짐. (=> 코드 수정 및 유지보수에 어려움)
+// 라우터(API)들을 다른 파일로 추출하는 것이 좋다.
+// 1) route 폴더 생성
+// 2) '/', '/user'로 들어오는 요청을 모아놓을 파일 생성
+
+
+
+
+// GET 라우터
+app.get('/', (req, res) => {
+  res.send('hihihihi')
 })
 
 
